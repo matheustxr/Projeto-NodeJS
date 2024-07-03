a656ba02b36f5fbe8f31a443bfa943ddfdb165ba
bmouseProductions <designers@bmouseproductions.com>
2024-07-03 16:43:23 -0300

    aula de query params

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,13 @@ app.get("/message/:id/:user", (request, response) => {
         Id da mensagem: ${id}.
         Para o usuário ${user}
     `);
-})
+});
+
+app.get("/users", (request, response) => {
+    const {page, limit} = request.query;
+
+    response.send(`Página: ${page}. Mostrar: ${limit}`)
+});
 
 const PORT = 3333;
 app.listen(PORT, () => {console.log(`Server is running in ${PORT}`)})
