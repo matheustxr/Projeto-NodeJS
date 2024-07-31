9209980c54cf86e33ae66dffeee15520eff8046e
Matheus Teixeira <matheustxr.dev@gmail.com>
2024-07-31 08:35:38 -0300

    validação da senha

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,5 +1,6 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
+const {compare} = require("bcryptjs")
 
 class sessionsController {
 	async create(request, response) {
@@ -8,6 +9,12 @@ class sessionsController {
 		const user = await knex("users").where({ email }).first();
 
 		if (!user) {
+            throw new AppError("Usuário ou senha inválidos", 401);
+        }
+
+		const passwordMatched =  await compare(password, user.password);
+
+		if (!passwordMatched) {
             throw new AppError("Usuário ou senha inválidos", 401);
         }
 
